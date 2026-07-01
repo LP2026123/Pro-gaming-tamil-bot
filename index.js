ec3c2cabe1fcbb33fb88d74cdadf04e4d2671832
LP2026123 <freefirepayalugada29@gmail.com>
2026-07-02 03:36:45 +0530

    Initialize Discord bot client with intents

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const TOKEN = process.env.TOKEN;
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is Online!`);
+});
+
+client.login(TOKEN);
